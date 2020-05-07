b115173cce2cc16af155e60910a945d72cab06d9
Alexander Cyon <alex.cyon@gmail.com>
2020-05-07 15:37:56 +0200

    First rought crude version of parsing atom, decreasing max particle count to 10, from 15, due to intermiediate state "transfers" taking up some memory.

--- app/src/atom_model/RadixAddress.c
+++ app/src/atom_model/RadixAddress.c
@@ -1,44 +1,47 @@
-#include "RadixAddress.h"
-#include <stdint.h>
-#include "libbase58.h"
+// #include "RadixAddress.h"
+// #include "libbase58.h"
 
-RadixAddressFromCborError from_cbor_to_RadixAddress(
-    const char *input_cbor_bytes,
-    const size_t input_cbor_byte_count,
-    RadixAddress *output_radixAddress
-) {
-    if (input_cbor_bytes != RADIX_ADDRESS_BYTE_COUNT) {
-        PRINTF("Too few bytes, expected: %d, but got: %d\n", RADIX_ADDRESS_BYTE_COUNT, input_cbor_bytes);
-        return RADIX_ADDRESS_FROM_CBOR_ERROR_TOO_FEW_BYTES;
-    }
+// RadixAddressFromCborError from_cbor_to_RadixAddress(
+//     const char *input_cbor_bytes,
+//     const size_t input_cbor_byte_count,
+//     RadixAddress *output_radixAddress
+// ) {
+//     if (input_cbor_bytes != RADIX_ADDRESS_BYTE_COUNT) {
+//         PRINTF("Too few bytes, expected: %d, but got: %d\n", RADIX_ADDRESS_BYTE_COUNT, input_cbor_bytes);
+//         return RADIX_ADDRESS_FROM_CBOR_ERROR_TOO_FEW_BYTES;
+//     }
 
-    RadixAddress address = {
-        .bytes = input_cbor_bytes
-    };
+//     RadixAddress address = {
+//         .bytes = input_cbor_bytes
+//     };
 
-    *output_radixAddress = address;
-    return RADIX_ADDRESS_NO_ERROR;
-}
+//     *output_radixAddress = address;
+//     return RADIX_ADDRESS_NO_ERROR;
+// }
 
-size_t to_string_radix_address(
-    RadixAddress *address,
-    char *output_buffer,
-    const size_t max_length
-) {
-    if (max_length < RADIX_ADDRESS_BYTE_COUNT + 1) { // + 1 for NULL
-        int exception = 0x9111;
-        PRINTF("Cannot write Radix address, too small buffer!, throwing error: %d\n", exception);
-        THROW(exception);
-    }
-    int string_length_incl_null;
-    char address_base58[max_length];
-    bool successful = b58enc(address_base58, &string_length_incl_null, address->bytes, RADIX_ADDRESS_BYTE_COUNT);
+// size_t to_string_radix_address(
+//     RadixAddress *address,
+//     char *output_buffer,
+//     const size_t max_length
+// ) {
     
-    if (!successful) {
-       int exception = 0x9112;
-        PRINTF("Failed to base58 encode data!, throwing error: %d\n", exception);
-        THROW(exception);
-    }
+//     if (max_length < RADIX_ADDRESS_BYTE_COUNT + 1) { // + 1 for NULL
+//         FAIL("Cannot write Radix address, too small buffer!, throwing error");
+//     }
 
-    return (size_t) string_length_incl_null;
-}
+//     size_t string_length_incl_null;
+//     char address_base58[max_length];
+    
+//     bool successful = b58enc(
+//         address_base58,
+//         &string_length_incl_null,
+//         address->bytes,
+//         RADIX_ADDRESS_BYTE_COUNT
+//     );
+    
+//     if (!successful) {
+//         FAIL("Failed to base58 encode data!");
+//     }
+
+//     return string_length_incl_null;
+// }
