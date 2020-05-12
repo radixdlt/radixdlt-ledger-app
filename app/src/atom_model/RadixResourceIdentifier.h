4fd36e440f40bdf03f9bac2c33deeb907eb40c44
Alexander Cyon <alex.cyon@gmail.com>
2020-05-12 13:48:59 +0200

    successfully decoded all transfers, I think.

--- app/src/atom_model/RadixResourceIdentifier.h
+++ app/src/atom_model/RadixResourceIdentifier.h
@@ -1,5 +1,7 @@
+#include <stdint.h>
+
 /* A Radix resource identifier is a human readable index into the Ledger which points to a name state machine */
 typedef struct {
     /* On format: `/:address/:name`, e.g.: `"/JH1P8f3znbyrDj8F4RWpix7hRkgxqHjdW2fNnKpR3v6ufXnknor/XRD"` */
-    char bytes[39]; // 39 (address) + 1 (slash) + 1-14 (Symbol) bytes, but we support unlimited length
+    uint8_t bytes[39]; // 39 (address) + 1 (slash) + 1-14 (Symbol) bytes, but we support unlimited length
 } RadixResourceIdentifier;
