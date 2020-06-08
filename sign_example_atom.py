932988a132b78d6a245c596ef8a1a5e15484bcc5
Alexander Cyon <alex.cyon@gmail.com>
2020-06-08 09:20:06 +0200

    Making Python test script dynamic, reading a predefined Test vector JSON file created by Swift library, which can create many different atoms with corresponding Particle Meta data and expected signature.

--- sign_example_atom.py
+++ sign_example_atom.py
@@ -2,153 +2,211 @@
 
 from ledgerblue.comm import getDongle
 from ledgerblue.commException import CommException
+from typing import List
 import argparse
 import struct
 import math
 import binascii
+import json
+import hashlib
 
 
-def expected_printed_output():
-	return """
-**************************************
-Transfer 1
-    recipient address: JFeqmatdMyjxNce38w3pEfDeJ9CV6NCkygDt3kXtivHLsP3p846
-    amount: 1 E-18
-    token (RRI): ZELDA
-
-Transfer 2
-    recipient address: JG3Ntbhj144hpz2ZooKsQG3Hq7UkCMwmFMwXfaYQgKFzNXAQvo5
-    amount: 2 E-18
-    token (RRI): ZELDA
-
-Transfer 3
-    recipient address: JFtJPDGvw4NDQyqCk7P5pWudNMeT8TFGCSvY9pTEqiyVhUGM9R9
-    amount: 3 E-18
-    token (RRI): ZELDA
-**************************************
-	"""
+STREAM_LEN = 255 # Stream in batches of STREAM_LEN bytes each.
 
 def bip32_path_big_endian_encoded():
 	# return b"\x80000002" + struct.pack(">I", 1, 3)
 	return bytes.fromhex("800000020000000100000003")
 
 
-# The private key for BIP32 path: <44'/536'/2'/1/3>
-# using mnemonic: <equip will roof matter pink blind book anxiety banner elbow sun young>
-def alice_private_key():
-	return "f423ae3097703022b86b87c15424367ce827d11676fae5c7fe768de52d9cce2e"
+class TestVector(object):
+	def __init__(self, j):
+		self.__dict__ = json.loads(j)
 
-# ByteInterval of the following fields in the following order:
-# [
-# 	  addressByteInterval,
-# 	  amountByteInterval,
-# 	  serializerByteInterval,
-# 	  tokenDefinitionReferenceByteInterval
-# ]
-def particle_meta_data():
-	return "01de0029020e0023029e002602de003d0369002903990023042900260469003d06b6002906e600230776002607b6003d0841002908710023090100260941003d0b8e00290bbe00230c4e00260c8e003d0d1900290d4900230dd900260e19003d00000000000000000f6b001800000000"
+	def description(self) -> str:
+		return self.descriptionOfTest
 
-def atom_6_transfP_1_message_part_by_alice():
-	return "bf686d65746144617461bf6974696d657374616d706d31353930343135363933303037ff6e7061727469636c6547726f75707384bf697061727469636c657383bf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e86c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e206776657273696f6e1864ffbf687061727469636c65bf67616464726573735827040202c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee57f51ade766616d6f756e7458210500000000000000000000000000000000000000000000000000000000000000016c64657374696e6174696f6e73815102e142e5bb89503e3210b1f2c893eb5c126b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ffbf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e76c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ff6a73657269616c697a65727472616469782e7061727469636c655f67726f75706776657273696f6e1864ffbf697061727469636c657383bf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e86c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e206776657273696f6e1864ffbf687061727469636c65bf67616464726573735827040202f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f925dbba8c66616d6f756e7458210500000000000000000000000000000000000000000000000000000000000000026c64657374696e6174696f6e7381510242ca0ec5e59f054eaabea5c37335bb096b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ffbf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e66c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ff6a73657269616c697a65727472616469782e7061727469636c655f67726f75706776657273696f6e1864ffbf697061727469636c657383bf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e86c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e206776657273696f6e1864ffbf687061727469636c65bf67616464726573735827040202e493dbf1c10d80f3581e4904930b1404cc6c13900ee0758474fa94abe8c4cd13fdc26f8866616d6f756e7458210500000000000000000000000000000000000000000000000000000000000000036c64657374696e6174696f6e73815102d761b817b7111bd27701a8f1ea60d3896b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ffbf687061727469636c65bf676164647265737358270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc3466616d6f756e7458210500000000000000000000000000000000000000000000000000000000000003e56c64657374696e6174696f6e7381510205f1e0f9fa6f2922f14827be92d0a2a06b6772616e756c61726974795821050000000000000000000000000000000000000000000000000000000000000001656e6f6e63650066706c616e636b1a019561046a73657269616c697a6572782472616469782e7061727469636c65732e7472616e736665727261626c655f746f6b656e737818746f6b656e446566696e6974696f6e5265666572656e6365583b062f4a45796f4b4e455961774a6b4e5469696e5168316852396333463537414e697879425269396673534566476564756d696666522f5a454c44416776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ff6a73657269616c697a65727472616469782e7061727469636c655f67726f75706776657273696f6e1864ffbf697061727469636c657381bf687061727469636c65bf65627974657357014f70656e2074686520706f642062617920646f6f72736c64657374696e6174696f6e7382510205f1e0f9fa6f2922f14827be92d0a2a05102b66146757bca0b4fc65cb3aed37a0e3d6466726f6d58270402026d5e07cfde5df84b5ef884b629d28d15b0f6c66be229680699767cd57c6182882a49dc34656e6f6e63651b000001724c1e87776a73657269616c697a65727772616469782e7061727469636c65732e6d65737361676562746f58270402022f8bde4d1a07209355b4a7250a5c5128e88b84bddc619ab7cba8d569b240efe4c58f581e6776657273696f6e1864ff6a73657269616c697a65727372616469782e7370756e5f7061727469636c65647370696e016776657273696f6e1864ff6a73657269616c697a65727472616469782e7061727469636c655f67726f75706776657273696f6e1864ff6776657273696f6e1864ff"
+	def transfers_human_readable(self) -> str:
+		return self.transfersHumanReadable
 
-def number_of_particles():
-	metadata_byte_count = len(particle_meta_data())
-	print(f"metadata_byte_count: {metadata_byte_count}")
-	number_of_particles = int(metadata_byte_count/32)
-	print(f"number of particles: {number_of_particles}")
-	return struct.pack(">B", number_of_particles)
+	def addresses(self) -> List[str]:
+		return self.atomDescription['allAddresses']
+
+	# The private key for BIP32 path: <44'/536'/2'/1/3>
+	# using mnemonic: <equip will roof matter pink blind book anxiety banner elbow sun young>
+	def alice_private_key(self) -> str:
+		return self.expected['privateKeyAlice']
+
+	# ByteInterval of the following fields in the following order:
+	# [
+	# 	  addressByteInterval,
+	# 	  amountByteInterval,
+	# 	  serializerByteInterval,
+	# 	  tokenDefinitionReferenceByteInterval
+	# ]
+	def particle_meta_data(self) -> bytearray:
+		return bytearray.fromhex(self.atomDescription['particleSpinUpMetaDataHex'])
+
+	def cbor_encoded_hex(self) -> str:
+		return self.atomDescription['cborEncodedHex']
+
+	def atom_cbor_encoded(self) -> bytearray:
+		return bytearray.fromhex(self.cbor_encoded_hex())
+
+	def atom_byte_count(self) -> int:
+		return len(self.cbor_encoded_hex())/2
+
+	def expected_hash_hex(self) -> str:
+		return self.expected['shaSha256HashOfAtomCborHex']
+
+	def expected_signature_rs_hex(self) -> str:
+		return self.expected['signatureRSOfAtomHashHex']
+
+	def up_particles_dict(self):
+		non_filtered_up_particle_count_dict = self.atomDescription['upParticles']
+		return { key:value for (key,value) in non_filtered_up_particle_count_dict.items() if value > 0 }
+
+	def number_of_up_particles(self) -> int:
+		return self.up_particles_dict()['totalCount']
+
+	def number_of_transferrable_tokens_particles_with_spin_up(self) -> int:
+		return self.up_particles_dict()['transferrableTokensParticles']
+
+	def contains_non_transfer_data(self) -> bool:
+		return (self.number_of_up_particles() - self.number_of_transferrable_tokens_particles_with_spin_up()) > 0
+
+	def apdu_prefix(self) -> bytearray:
+		# https://en.wikipedia.org/wiki/Smart_card_application_protocol_data_unit
+		CLA = bytes.fromhex("AA")
+		INS = b"\x02" # `02` is command "SIGN_ATOM"
+		P1 = struct.pack(">B", self.number_of_up_particles())
+		P2 = b"\x00"
+
+		return CLA + INS + P1 + P2
+
+	def particle_group_count(self) -> int:
+		return self.atomDescription['particleGroupCount']
 
 
-def apduPrefix():
-	# https://en.wikipedia.org/wiki/Smart_card_application_protocol_data_unit
-	CLA = bytes.fromhex("AA")
-	INS = b"\x02" # `02` is sign atom
-	P1 = number_of_particles()
-	P2 = b"\x00"
+def send_large_atom_to_ledger_in_many_chunks(vector: TestVector):
+	print(
+		"""
+🚀 Streaming Atom from vector to Ledger:
+'{}'
+Atom byte count: #{}bytes
+Particle groups: #{}
+Particles with spin UP: #{}
+Contains non transfer data: {}
+Transfers: {}
+		""".format(
+			vector.description(), 
+			vector.atom_byte_count(),
+			vector.particle_group_count(),
+			vector.up_particles_dict(),
+			vector.contains_non_transfer_data(),
+			vector.transfers_human_readable()
+		)
+	)
 
-	return CLA + INS + P1 + P2
+	bip_32_path_bytes = bip32_path_big_endian_encoded()
+	particles_meta_data_bytes = vector.particle_meta_data()
+	atom_bytes = vector.atom_cbor_encoded()
+
+	hasher = hashlib.sha256()
+	hasher.update(atom_bytes)
+	first_digest = hasher.digest()
+	hasher = hashlib.sha256()
+	hasher.update(first_digest)
+	hash_of_atom = hasher.hexdigest()
+
+	if hash_of_atom == vector.expected_hash_hex():
+		print("\n✅ Awesome! Calculated hash matches the expected one that from Swift library ✅\n")
+	else:
+		print("\n ☢️ Hash mismatch ☢️\n")
+		print(f"Expected hash: {vector.expected_hash_hex()}") 
+		print(f"But calculated hash: {hash_of_atom}")
 
 
-def send_large_atom_to_ledger_in_many_chunks():
+	atom_byte_count = len(atom_bytes)
+	assert(atom_byte_count == vector.atom_byte_count())
 
-	STREAM_LEN = 255 # Stream in batches of STREAM_LEN bytes each.
-	bip32Bytes = bip32_path_big_endian_encoded()
-	particlesMetaDataBytes = bytearray.fromhex(particle_meta_data())
-	atomBytes = bytearray.fromhex(atom_6_transfP_1_message_part_by_alice())
+	atom_byte_count_encoded = struct.pack(">h", atom_byte_count) # `>` means big endian, `h` means `short` -> 2 bytes
 
-	atomByteCount = len(atomBytes)
+	prefix = vector.apdu_prefix()
 
-	print("atomByteCount: " + str(atomByteCount))
-
-	atomByteCountEncoded = struct.pack(">h", atomByteCount) # `>` means big endian, `h` means `short` -> 2 bytes
-	print("atomByteCountEncoded: " + atomByteCountEncoded.hex())
-
-	prefix = apduPrefix()
-
-	payload = bip32Bytes + atomByteCountEncoded + particlesMetaDataBytes
+	payload = bip_32_path_bytes + atom_byte_count_encoded + particles_meta_data_bytes
 
 	print("Sending payload: " + payload.hex())
+
 
 	L_c = bytes([len(payload)])
 	apdu = prefix + L_c + payload
 
-	dongle = getDongle(True)
+	letDongleOutputDebugPrintStatements = False
+	dongle = getDongle(debug=letDongleOutputDebugPrintStatements)
 	result = dongle.exchange(apdu)
 
-	numberOfBytesThatHaveBeenSentToLedger = 0
+	count_bytes_sent_to_ledger = 0
 
-	chunkIndex = 0
-	numberOfChunksToSend = int(math.ceil(atomByteCount / STREAM_LEN))
-	print(f"Atom will be sent in #chunks: {numberOfChunksToSend}")
+	chunk_index = 0
+	number_of_chunks_to_send = int(math.ceil(atom_byte_count / STREAM_LEN))
+	print(f"Atom will be sent in #chunks: {number_of_chunks_to_send}")
 
-	atomBytesChunked = atomBytes.copy()
+	atom_bytes_chunked = atom_bytes.copy()
 
 	# Keep streaming data into the device till we run out of it.
-	while numberOfBytesThatHaveBeenSentToLedger < atomByteCount:
-		print(f"Sending chunk {chunkIndex+1}/{numberOfChunksToSend}")
-		numberOfBytesLeftToSend = atomByteCount - numberOfBytesThatHaveBeenSentToLedger
+	while count_bytes_sent_to_ledger < atom_byte_count:
+		number_of_bytes_left_to_send = atom_byte_count - count_bytes_sent_to_ledger
 
 		chunk = bytearray(0)
-		if numberOfBytesLeftToSend > STREAM_LEN:
-			chunk = atomBytesChunked[0:STREAM_LEN]
-			atomBytesChunked = atomBytesChunked[STREAM_LEN:]
+		if number_of_bytes_left_to_send > STREAM_LEN:
+			chunk = atom_bytes_chunked[0:STREAM_LEN]
+			atom_bytes_chunked = atom_bytes_chunked[STREAM_LEN:]
 		else:
-			chunk = atomBytesChunked
-			atomBytesChunked = bytearray(0)
+			chunk = atom_bytes_chunked
+			atom_bytes_chunked = bytearray(0)
 
-		chunkSize = len(chunk)
-		print(f"Chunk {chunkIndex+1}: [{numberOfBytesThatHaveBeenSentToLedger}-{numberOfBytesThatHaveBeenSentToLedger+chunkSize}]") # has size: {chunkSize}, bytes: {chunk.hex()}")
-		# hasher.update(chunk)
-		# print(f"Expected hasher state after chunk {chunkIndex+1}: {hasher.hexdigest()}")
-		L_c = bytes([chunkSize])
-		numberOfBytesThatHaveBeenSentToLedger += chunkSize
+		chunk_size = len(chunk)
+		print(f"Chunk {chunk_index+1}: [{count_bytes_sent_to_ledger}-{count_bytes_sent_to_ledger+chunk_size}]")
+		L_c = bytes([chunk_size])
+		count_bytes_sent_to_ledger += chunk_size
 		apdu = prefix + L_c + chunk
 		result = dongle.exchange(apdu)
-		chunkIndex += 1
-		print(f"numberOfBytesThatHaveBeenSentToLedger: {numberOfBytesThatHaveBeenSentToLedger}, atomByteCount: {atomByteCount}")
+		chunk_index += 1
 
-	responseFromLedgerContainingSignature = result.hex()
-	signatureFromLedger = responseFromLedgerContainingSignature[2:130]
+	response_from_ledger_device_w_signature = result.hex()
+	signature_from_ledger_device = response_from_ledger_device_w_signature[2:130]
 
 	
-	expectedSignatureHex = "1f4b599e8983345745d76eefda8edec0b300604c11115e4e79bc3275acb40a884f8e6d91204aea49b51d3ba497a4cbce9d5cd4d008608fdf4e741ee948f910e1"
-	expectedSignature = bytearray.fromhex(expectedSignatureHex)
+	expected_signature_hex = vector.expected_signature_rs_hex()
 
-	if expectedSignatureHex == signatureFromLedger:
+	if expected_signature_hex == signature_from_ledger_device:
 		print("\n✅ Awesome! Signature from ledger matches that from Swift library ✅\n")
 	else:
 		print("\n ☢️ Signature mismatch ☢️\n")
-		print(f"Expected signature: {expectedSignatureHex}") 
-		print(f"But got signature from ledger: {signatureFromLedger}")
+		print(f"Expected signature: {expected_signature_hex}") 
+		print(f"But got signature from ledger: {signature_from_ledger_device}")
 
 	# print("Length: " + str(len(result)))
 
 	print("Expected to to parse these transfers:\n")
-	print(expected_printed_output())
+	print(vector.expected_printed_output())
 
 if __name__ == "__main__":
-	# parser = argparse.ArgumentParser()
-	#parser.add_argument('--txnJson', '-j', type=str, required=False)
-	# parser.add_argument('--index', '-i', type=int, required=True)
-	# args = parser.parse_args()
-	send_large_atom_to_ledger_in_many_chunks()
+	parser = argparse.ArgumentParser(description="Stream CBOR encoded atom to Ledger for signing.")
+	
+	parser.add_argument(
+		'--vectorJson', 
+		'-j', 
+		default='./TestVectors/WithNonTransferData/WithTransfers/MultipleTransfers/vector.json',
+		type=str, 
+		# required=True,
+		help='Path to JSON file containing test vector with CBOR encoded Atom, the particle meta data, description of atom contents and expected hash and signature.\n\nDefaults to %(default)',
+		metavar='FILE'
+	)
+
+	args = parser.parse_args()
+	json_file_path = args.vectorJson
+
+	with open(json_file_path) as json_file:
+		vector = TestVector(json_file.read())
+		send_large_atom_to_ledger_in_many_chunks(vector=vector)
