8bc87ceccb705a0d4340b23aba67e8fbbfe61a4d
Alexander Cyon <alex.cyon@gmail.com>
2020-06-18 13:58:53 +0200

    improve security when signing

--- app/src/key_and_signatures.h
+++ app/src/key_and_signatures.h
@@ -7,16 +7,11 @@ int parse_bip32_path_from_apdu_command(
     unsigned short output_bip32PathString_length
 );
 
-// Convert un-compressed zilliqa public key to a compressed form.
-void compress_public_key(cx_ecfp_public_key_t *publicKey);
-
 // derive_radix_key_pair derives a key pair from a BIP32 path and the Ledger
 // seed. Returns the public key and private key if not NULL.
-void derive_radix_key_pair(
-    uint32_t *bip32path, 
-    cx_ecfp_public_key_t *publicKey,
-    cx_ecfp_private_key_t *privateKey_nullable
-);
+void derive_radix_key_pair(uint32_t *bip32path,
+                           volatile cx_ecfp_public_key_t *publicKey,
+                           volatile cx_ecfp_private_key_t *privateKey_nullable);
 
 size_t derive_sign_move_to_global_buffer(
     uint32_t *bip32path, 
