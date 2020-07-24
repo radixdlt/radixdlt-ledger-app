9c63e4ddc70783373be16aa2e110bf2030559b96
Alexander Cyon <alex.cyon@gmail.com>
2020-07-24 14:41:37 +0200

    broken uncompress_pub_key

--- app/src/encryption/ecies.h
+++ app/src/encryption/ecies.h
@@ -2,24 +2,6 @@
 #include "stdint.h"
 #include "os.h"
 
-size_t ecies_encrypt(
-    const uint8_t* data_in,
-    const size_t data_in_len,
-    uint8_t* data_out,
-    size_t data_out_len,
-    const uint8_t* uncompress_public_key_bytes,
-    const size_t pk_byte_count);
-
-size_t ecies_encrypt_iv(
-    const uint8_t* iv,
-    const size_t iv_len,
-    const uint8_t* data_in,
-    const size_t data_in_len,
-    uint8_t* data_out,
-    size_t data_out_len,
-    const uint8_t* uncompress_public_key_bytes,
-    const size_t pk_byte_count);
-
 size_t ecies_decrypt_bipPath(
     const uint8_t* data_in,
     const size_t data_in_len,
